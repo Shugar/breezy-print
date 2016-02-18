ab17d3c0e2ef34656ee27ed5daba86db7f29ccfc
Tigran Tumasov <shugar348@gmail.com>
2016-02-18 16:57:56 +0300

    change dropzone to dropify

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,191 @@
-!function(){"use strict";var e="undefined"==typeof window?global:window;if("function"!=typeof e.require){var n={},t={},r={},i={}.hasOwnProperty,o="components/",c=function(e,n){var t=0;n&&(0===n.indexOf(o)&&(t=o.length),n.indexOf("/",t)>0&&(n=n.substring(t,n.indexOf("/",t))));var i=r[e+"/index.js"]||r[n+"/deps/"+e+"/index.js"];return i?o+i.substring(0,i.length-".js".length):e},s=/^\.\.?(\/|$)/,u=function(e,n){for(var t,r=[],i=(s.test(n)?e+"/"+n:n).split("/"),o=0,c=i.length;c>o;o++)t=i[o],".."===t?r.pop():"."!==t&&""!==t&&r.push(t);return r.join("/")},l=function(e){return e.split("/").slice(0,-1).join("/")},a=function(n){return function(t){var r=u(l(n),t);return e.require(r,n)}},d=function(e,n){var r={id:e,exports:{}};return t[e]=r,n(r.exports,a(e),r),r.exports},f=function(e,r){var o=u(e,".");if(null==r&&(r="/"),o=c(e,r),i.call(t,o))return t[o].exports;if(i.call(n,o))return d(o,n[o]);var s=u(o,"./index");if(i.call(t,s))return t[s].exports;if(i.call(n,s))return d(s,n[s]);throw new Error('Cannot find module "'+e+'" from "'+r+'"')};f.alias=function(e,n){r[n]=e},f.register=f.define=function(e,t){if("object"==typeof e)for(var r in e)i.call(e,r)&&(n[r]=e[r]);else n[e]=t},f.list=function(){var e=[];for(var t in n)i.call(n,t)&&e.push(t);return e},f.brunch=!0,f._cache=t,e.require=f}}(),require.register("initialize",function(e,n,t){$(document).ready(function(){return Dropzone.autoDiscover=!1,$(".firstScreen-dropzone").dropzone({url:"/file/post",autoProcessQueue:!1}),$(".stepOneButton").click(function(){return $(".firstScreen-stepOne").hide(),$(".firstScreen-stepTwo").show()}),$(".firstScreen-back").click(function(){return $(".firstScreen-stepTwo").hide(),$(".firstScreen-stepOne").show()}),$(".secondScreen-back").click(function(){return $("#release").hide(),$("#start").show()}),$(".secondScreen-printerChange").click(function(){return $(".secondScreen-printerStepTwo").hide(),$(".secondScreen-printerStepOne").show(),$(".secondScreen-print").removeClass("secondScreen-printActive"),$(".secondScreen-next").show()}),$(".secondScreen-next").click(function(){return $(".secondScreen-printerStepOne").hide(),$(".secondScreen-printerStepTwo").show(),$(this).hide(),$(".secondScreen-print").addClass("secondScreen-printActive")}),$(".secondScreen-cancel").click(function(){return swal({title:"Are you sure?",text:"You will not be able to recover this document!",type:"warning",showCancelButton:!0,confirmButtonColor:"#DD6B55",confirmButtonText:"Yes, delete it!",closeOnConfirm:!1},function(){return swal({title:"Deleted!",text:"Your document has been deleted.",type:"success"},function(){return document.location="/"})})}),$("#btn-start-next").click(function(){return $("#start").hide(),$("#release").show(),$("#file-info").html($("#docUpload").val())}),$("#btn-release-next").click(function(){return $.ajax({dataType:"json",url:"endpoint?shortCode="+$("#txt-shortcode").val(),success:function(e){return $("#select-printer").hide(),$("#set-printer-settings").show(),$("#hid-endpointId").val(e.endpoint_id),$("#printer-name").html(e.display_name),$("#btn-release-next").hide(),$("#btn-print").show()}})}),$("#btn-print-another").click(function(){return document.location="/",$(this).hide()}),$("form").ajaxForm({success:function(){return $("#action-panel").hide(),$("#print-another-panel").show()}})})});
+(function() {
+  'use strict';
+
+  var globals = typeof window === 'undefined' ? global : window;
+  if (typeof globals.require === 'function') return;
+
+  var modules = {};
+  var cache = {};
+  var aliases = {};
+  var has = ({}).hasOwnProperty;
+
+  var endsWith = function(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+  };
+
+  var _cmp = 'components/';
+  var unalias = function(alias, loaderPath) {
+    var start = 0;
+    if (loaderPath) {
+      if (loaderPath.indexOf(_cmp) === 0) {
+        start = _cmp.length;
+      }
+      if (loaderPath.indexOf('/', start) > 0) {
+        loaderPath = loaderPath.substring(start, loaderPath.indexOf('/', start));
+      }
+    }
+    var result = aliases[alias + '/index.js'] || aliases[loaderPath + '/deps/' + alias + '/index.js'];
+    if (result) {
+      return _cmp + result.substring(0, result.length - '.js'.length);
+    }
+    return alias;
+  };
+
+  var _reg = /^\.\.?(\/|$)/;
+  var expand = function(root, name) {
+    var results = [], part;
+    var parts = (_reg.test(name) ? root + '/' + name : name).split('/');
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
+    }
+    return results.join('/');
+  };
+
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function expanded(name) {
+      var absolute = expand(dirname(path), name);
+      return globals.require(absolute, path);
+    };
+  };
+
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    cache[name] = module;
+    definition(module.exports, localRequire(name), module);
+    return module.exports;
+  };
+
+  var require = function(name, loaderPath) {
+    var path = expand(name, '.');
+    if (loaderPath == null) loaderPath = '/';
+    path = unalias(name, loaderPath);
+
+    if (has.call(cache, path)) return cache[path].exports;
+    if (has.call(modules, path)) return initModule(path, modules[path]);
+
+    var dirIndex = expand(path, './index');
+    if (has.call(cache, dirIndex)) return cache[dirIndex].exports;
+    if (has.call(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
+
+    throw new Error('Cannot find module "' + name + '" from '+ '"' + loaderPath + '"');
+  };
+
+  require.alias = function(from, to) {
+    aliases[to] = from;
+  };
+
+  require.register = require.define = function(bundle, fn) {
+    if (typeof bundle === 'object') {
+      for (var key in bundle) {
+        if (has.call(bundle, key)) {
+          modules[key] = bundle[key];
+        }
+      }
+    } else {
+      modules[bundle] = fn;
+    }
+  };
+
+  require.list = function() {
+    var result = [];
+    for (var item in modules) {
+      if (has.call(modules, item)) {
+        result.push(item);
+      }
+    }
+    return result;
+  };
+
+  require.brunch = true;
+  require._cache = cache;
+  globals.require = require;
+})();
+require.register("scripts/initialize", function(exports, require, module) {
+$(document).ready(function() {
+  $('.dropify').dropify();
+  $('.stepOneButton').click(function() {
+    $('.firstScreen-stepOne').hide();
+    return $('.firstScreen-stepTwo').show();
+  });
+  $('.firstScreen-back').click(function() {
+    $('.firstScreen-stepTwo').hide();
+    return $('.firstScreen-stepOne').show();
+  });
+  $('.secondScreen-back').click(function() {
+    $('#release').hide();
+    return $('#start').show();
+  });
+  $('.secondScreen-printerChange').click(function() {
+    $('.secondScreen-printerStepTwo').hide();
+    $('.secondScreen-printerStepOne').show();
+    $('.secondScreen-print').removeClass('secondScreen-printActive');
+    return $('.secondScreen-next').show();
+  });
+  $('.secondScreen-next').click(function() {
+    $('.secondScreen-printerStepOne').hide();
+    $('.secondScreen-printerStepTwo').show();
+    $(this).hide();
+    return $('.secondScreen-print').addClass('secondScreen-printActive');
+  });
+  $('.secondScreen-cancel').click(function() {
+    return swal({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this document!',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#DD6B55',
+      confirmButtonText: 'Yes, delete it!',
+      closeOnConfirm: false
+    }, function() {
+      return swal({
+        title: 'Deleted!',
+        text: 'Your document has been deleted.',
+        type: 'success'
+      }, function() {
+        return document.location = '/';
+      });
+    });
+  });
+  $('#btn-start-next').click(function() {
+    $('#start').hide();
+    $('#release').show();
+    return $('#file-info').html($('#docUpload').val());
+  });
+  $('#btn-release-next').click(function() {
+    return $.ajax({
+      dataType: 'json',
+      url: 'endpoint?shortCode=' + $('#txt-shortcode').val(),
+      success: function(data) {
+        $('#select-printer').hide();
+        $('#set-printer-settings').show();
+        $('#hid-endpointId').val(data.endpoint_id);
+        $('#printer-name').html(data.display_name);
+        $('#btn-release-next').hide();
+        return $('#btn-print').show();
+      }
+    });
+  });
+  $('#btn-print-another').click(function() {
+    document.location = '/';
+    return $(this).hide();
+  });
+  return $('form').ajaxForm({
+    success: function() {
+      $('#action-panel').hide();
+      return $('#print-another-panel').show();
+    }
+  });
+});
+});
+
+;
+//# sourceMappingURL=app.js.map
